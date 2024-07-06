ca7a5adb7c68be4811b5d3f82ec4ae872bc68705
janumaruku <jean-marc.kottin@epitech.eu>
2024-07-06 07:41:15 +0100

    Fix: Check the return value of the function execve in the child process

--- srcs/shell_functions/cli_execution/execute.c
+++ srcs/shell_functions/cli_execution/execute.c
@@ -13,7 +13,7 @@ int runner(char *path, char **cmd)
     int child = fork();
 
     if (child == 0) {
-        if (execve(path, cmd, t_env)) {
+        if (!execve(path, cmd, t_env)) {
             perror(cmd[0]);
             free_2d_array(t_env);
             clear_list(&l_env, str_pop);
