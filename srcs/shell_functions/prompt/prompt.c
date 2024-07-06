93905570cf15cf032ea6804a6376036f4586ec09
janumaruku <jean-marc.kottin@epitech.eu>
2024-07-06 07:40:11 +0100

    Fix: Change the name of the variable e_cmd to exit_cmd

--- srcs/shell_functions/prompt/prompt.c
+++ srcs/shell_functions/prompt/prompt.c
@@ -13,16 +13,16 @@ char **prompt(void)
     char *line = NULL;
     size_t l = 0;
     int g = 0;
-    char **e_cmd = NULL;
+    char **exit_cmd = NULL;
     char **cmd = NULL;
 
     my_putstr("[janumaruku] user>");
     g = getline(&line, &l, stdin);
     if (g == -1) {
-        e_cmd = malloc(sizeof(char *) * 2);
-        e_cmd[0] = my_strdup("exit");
-        e_cmd[1] = NULL;
-        my_exit(e_cmd);
+        exit_cmd = malloc(sizeof(char *) * 2);
+        exit_cmd[0] = my_strdup("exit");
+        exit_cmd[1] = NULL;
+        my_exit(exit_cmd);
     }
     if (is_empty_line(line))
         return NULL;
