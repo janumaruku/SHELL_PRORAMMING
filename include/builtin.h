601973ba03a135d177cb1231cfc0d16de7d4a165
janumaruku <jean-marc.kottin@epitech.eu>
2024-07-11 11:28:47 +0100

    Add: add a header file for builtin command functions

---
+++ include/builtin.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** SHELL PROGRAMMING
+** File description:
+** Builtin command function's prototypes
+*/
+
+int builtin(char **cmd);
+
+int my_env(char **cmd);
+
+int my_exit(char **cmd);
+
+int my_setenv(char **cmd);
+
+#pragma once
