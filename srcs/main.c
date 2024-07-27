a0bc689e940cd76f42f465ed2e9054a04cb289f2
janumaruku <jean-marc.kottin@epitech.eu>
2024-07-27 09:09:39 +0100

    Feat: update the main function
    Add a new variable, job_list of type "jobs_t *", used to store all jobs running in background

--- srcs/main.c
+++ srcs/main.c
@@ -8,15 +8,18 @@
 #include "../include/linked_list.h"
 #include "../include/shell.h"
 #include "../include/utils.h"
+#include "../include/job_control.h"
 
 plist_t l_env;
 char **t_env;
 char *past_dir;
+jobs_t *job_list;
 
 int main(int argc, char *argv[], char *env[])
 {
     l_env = tab_to_list(env);
     t_env = tab_dup(env);
+    job_list = init_job_list();
     past_dir = NULL;
     main_loop();
     return 0;
