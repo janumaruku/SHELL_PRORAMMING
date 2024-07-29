5767ac41a566b04a1b84a6452085b4181ede9c87
janumaruku <jean-marc.kottin@epitech.eu>
2024-07-29 19:48:12 +0100

    Feat: change the child pid variable from  to , declared in a global scope

--- srcs/shell_functions/cli_execution/execute.c
+++ srcs/shell_functions/cli_execution/execute.c
@@ -13,12 +13,12 @@ int cur_process;
 void job_control(int signum)
 {
     if (signum == SIGINT) {
-        printf("\n");
         kill(cur_process, SIGINT);
+        printf("\n");
     }
     if (signum == SIGTSTP) {
-        printf("\n===> Suspended\n");
         kill(cur_process, SIGSTOP);
+        printf("\nSuspended\n");
     }
 }
 
@@ -33,18 +33,17 @@ void set_sigaction(struct sigaction *sa)
 
 int runner(char *path, char **cmd)
 {
-    int child = fork();
+    cur_process = fork();
     struct sigaction sa;
 
     set_sigaction(&sa);
-    cur_process = child;
-    if (child == 0) {
+    if (cur_process == 0) {
         if (!execve(path, cmd, t_env)) {
             perror(cmd[0]);
             free_2d_array(t_env);
             clear_list(&l_env, str_pop);
         }        
     }
-    waitpid(child, 0, WUNTRACED);
+    waitpid(cur_process, 0, WUNTRACED);
     return 0;
 }
