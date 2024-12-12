4534c0104db9f514bb7a7667f981e5c124be2d5f
janumaruku <jean-marc.kottin@epitech.eu>
2024-12-12 18:06:47 +0100

    fix: resolve some memory errors

--- srcs/main.c
+++ srcs/main.c
@@ -119,7 +119,7 @@ int main(int argc, char *argv[], char *env[])
     // insert_word(&root, "flow");
     // insert_word(&root, "flight");
     // print_trie(root);
-    char *str = my_strdup("tree || ls > data && cat data || cat res && clear");
+    char *str = my_strdup("ls ; ls -l | cat > data ; cat < test");
     printf("%s\n\n", str);
     tree_t *tr = semicolon_parsing(str);
     print_tree_prefix(tr, func2); my_putchar('\n');
