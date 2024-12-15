0fb57b125fdecc17b3a2dd93608990a74a73f3e0
janumaruku <jean-marc.kottin@epitech.eu>
2024-12-15 02:05:43 +0100

    add: add processing function to go through the command line and execute all commands separated by any seperator

---
+++ srcs/shell_functions/cli_execution/processing.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** SHELL PROGRAMMING
+** File description:
+** Functions to execute a command line
+*/
+
+#include "../../../include/linked_list.h"
+#include "../../../include/utils.h"
+#include "../../../include/shell.h"
+#include "../../../include/prompt.h"
+#include "../../../include/builtin.h"
+#include "../../../include/binary_tree.h"
+#include "../../../include/command_parsing.h"
+
+int processing(tree_t *tree)
+{
+    char **cmd = NULL;
+    if (tree == NULL)
+        return 0;
+    if (my_strcmp(tree->data, ";") == 0) {
+        processing(tree->tleft);
+        processing(tree->tright);
+        return 0;
+    }
+    cmd = split(tree->data, cmd_seg);
+    interpretor(cmd);
+    free_2d_array(cmd);
+    return 0;
+}
