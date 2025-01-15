a82d360ccecf96fe018c883a5a9af1091d71673c
janumaruku <jean-marc.kottin@epitech.eu>
2025-01-15 14:00:33 +0100

    fix: delete useless comment in the main.c file

--- srcs/main.c
+++ srcs/main.c
@@ -21,79 +21,6 @@ jobs_t *job_list;
 extern pnode_t top_reached;
 extern pnode_t buttom_reached;
 
-// #include <stdbool.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// typedef struct trienode {
-//     bool terminal;
-//     struct trienode *children[255];
-// } trienode;
-
-// trienode *create_trienode(void) {
-//     trienode *node = malloc(sizeof(*node));
-
-//     node->terminal = false;
-//     for (int i = 0; i < 255; i++) {
-//         node->children[i] = NULL;
-//     }
-//     return node;
-// }
-
-// bool insert_word(trienode **root, char *word) {
-//     trienode *tmp = NULL;
-//     if (*root == NULL) {
-//         *root = create_trienode();
-//     }
-//     tmp = *root;
-
-//     for (int i = 0; word[i]; i++) {
-//         if (tmp->children[word[i]] == NULL) {
-//             tmp->children[word[i]] = create_trienode();
-//         }
-//         tmp = tmp->children[word[i]];
-//     }
-//     tmp->terminal = true;
-// }
-
-// void print_trienode_rec(trienode *node, char *str, int len) {
-//     char new_str[201];
-//     memcpy(new_str, str, len);
-//     new_str[len + 1] = 0;
-
-//     if (node->terminal)
-//         printf("word: %s\n", str);
-//     for (int i = 0; i < 255; i++) {
-//         if (node->children[i] != NULL) {
-//             new_str[len] = i;
-//             print_trienode_rec(node->children[i], new_str, len + 1);
-//         }
-//     }
-// }
-
-// void print_trie(trienode *root) {
-//     trienode *tmp = root;
-//     if (root == NULL)
-//         return;
-    
-//     for (int i = 0; i < 255; i++) {
-//         if (tmp->children[i] != NULL)
-//             print_trienode_rec(tmp, NULL, 0);
-//     }
-// }
-
-// void clear_trie(trienode **root) {
-    
-// }
-
-// void func1(void *data)
-// {
-//     char *d = (char *)data;
-
-//     free(d);
-// }
-
 void func2(void *data)
 {
     char *d = (char *)data;
@@ -112,20 +39,6 @@ int main(int argc, char *argv[], char *env[])
     top_reached = fill_node(my_strdup("top"));
     buttom_reached = fill_node(my_strdup("buttom"));
     main_loop();
-    // trienode *root = create_trienode();
-    // insert_word(&root, "macaco");
-    // insert_word(&root, "macaroon");
-    // insert_word(&root, "machine");
-    // insert_word(&root, "flower");
-    // insert_word(&root, "flow");
-    // insert_word(&root, "flight");
-    // print_trie(root);
-    // char *str = my_strdup("(echo \"DATE: \" ; date) > file");
-    // printf("%s\n\n", str);
-    // tree_t *tr = semicolon_parsing(str);
-    // print_tree_prefix(tr, func2); my_putchar('\n');
-    // clean_tree(tr, func1);
-    // free(str);
     return 0;
 }
 
