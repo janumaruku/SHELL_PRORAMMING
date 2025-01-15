6182a546b7b342e4d8a4004b4b1bd84828ceb8e8
janumaruku <jean-marc.kottin@epitech.eu>
2025-01-15 12:47:37 +0100

    feat: add functions to handle command executed inside or outside the the shell using istty function

--- srcs/shell_functions/main_loop/main_loop.c
+++ srcs/shell_functions/main_loop/main_loop.c
@@ -23,6 +23,42 @@ void func1(void *data)
     free(d);
 }
 
+int ctrl_d(int g, char *line)
+{
+    char **cmd = malloc(sizeof(char *) * 2);
+    int i = 0;
+
+    cmd[0] = my_strdup("exit");
+    cmd[1] = 0;
+    if (g == -1) {
+        free(line);
+        my_exit(cmd);
+    }
+    free_2d_array(cmd);
+    for (; (line[i] == ' ' || line[i] == '\t'); i++);
+    if (line[i] == '\n')
+        return 1;
+    return g;
+}
+
+char *esssai(void)
+{
+    int g = 0;
+    size_t k = 0;
+    char *line = NULL;
+
+    if (isatty(0) == 1)
+    my_putstr("$> ");
+    g = getline(&line, &k, stdin);
+    g = ctrl_d(g, line);
+    if (g == 1) {
+        free(line);
+        return NULL;
+    }
+    line[g - 1] = '\0';
+    return line;
+}
+
 int main_loop(void)
 {
     char **cmd = NULL;
@@ -30,12 +66,15 @@ int main_loop(void)
     command_tree = NULL;
     cli = NULL;
     while (1) {
-        cli = prompt2();
+        if (!isatty(0))
+            cli = esssai();
+        else
+            cli = prompt2();
         if (!cli)
             continue;
-        history = push_back(history, new_command(my_strdup(cli)));
         command_tree = semicolon_parsing(cli);
         processing(command_tree);
+        history = push_back(history, new_command(my_strdup(cli)));
         free(cli);
         cli = NULL;
         clean_tree(command_tree, str_pop);
